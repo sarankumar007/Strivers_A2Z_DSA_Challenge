7c0841a2d16ad37fdc11f8bc84c5cb7af54944de
Saran Kumar R <93342856+sarankumar007@users.noreply.github.com>
2023-05-05 23:31:02 +0530

    Create SearchLinkedList.java

---
+++ LinkedList/SearchLinkedList.java
@@ -0,0 +1,16 @@
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        while(head!=null){
+            if(head.data==key) return true;
+            head=head.next;
+        }
+        return false;
+    }
+}
